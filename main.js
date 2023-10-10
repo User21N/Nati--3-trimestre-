5741d73023af0e31cd142a69a6e33719d9d7f12e
User21N <114448391+User21N@users.noreply.github.com>
2023-10-10 07:41:02 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,5 +2,7 @@ function tocaSomPom () {
 document.querySelector('#som_tecla_pom').play();
 }
 document.querySelector('.tecla_pom');
+
 document.querySelectorAll('.tecla');
+
 const listaDeTeclas = document.querySelectorAll('.tecla');
